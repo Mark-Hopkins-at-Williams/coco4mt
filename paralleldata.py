736c149c1d7807a26cce63ff61e03cd5da49f34c
Mark Hopkins <mh24@williams.edu>
2023-07-24 15:08:04 -0400

    Integrated NLLB file reading into paralleldata.py.

--- paralleldata.py
+++ paralleldata.py
@@ -1,6 +1,8 @@
 from datasets import Dataset, DatasetDict
 from pandas import DataFrame
 
+DATA_DIR = "/home/data"
+
 code3 = {
         "en": "eng",
         "de": "deu",
@@ -38,27 +40,27 @@ def coco_data(src, tgt, lines=None):
     always used).    
     
     """
-    train_corpora = {'eng': enumerate_lines("/home/data/coco4mt-shared-task/hr_dataset/eng/train.txt"),
-                     'deu': enumerate_lines("/home/data/coco4mt-shared-task/hr_dataset/deu/train.txt"),
-                     'ind': enumerate_lines("/home/data/coco4mt-shared-task/hr_dataset/ind/train.txt"),
-                     'kor': enumerate_lines("/home/data/coco4mt-shared-task/hr_dataset/kor/train.txt"),
-                     'fra': enumerate_lines("/home/data/coco4mt-shared-task/lr_dataset/fra/train.txt"),
-                     'mya': enumerate_lines("/home/data/coco4mt-shared-task/lr_dataset/mya/train.txt"),
-                     'guj': enumerate_lines("/home/data/coco4mt-shared-task/lr_dataset/guj/train.txt")}
-    valid_corpora = {'eng': enumerate_lines("/home/data/coco4mt-shared-task/hr_dataset/eng/dev.txt"),
-                     'deu': enumerate_lines("/home/data/coco4mt-shared-task/hr_dataset/deu/dev.txt"),
-                     'ind': enumerate_lines("/home/data/coco4mt-shared-task/hr_dataset/ind/dev.txt"),
-                     'kor': enumerate_lines("/home/data/coco4mt-shared-task/hr_dataset/kor/dev.txt"),
-                     'fra': enumerate_lines("/home/data/coco4mt-shared-task/lr_dataset/fra/dev.txt"),
-                     'mya': enumerate_lines("/home/data/coco4mt-shared-task/lr_dataset/mya/dev.txt"),
-                     'guj': enumerate_lines("/home/data/coco4mt-shared-task/lr_dataset/guj/dev.txt")}
-    test_corpora  = {'eng': enumerate_lines("/home/data/coco4mt-shared-task/hr_dataset/eng/test.txt"),
-                     'deu': enumerate_lines("/home/data/coco4mt-shared-task/hr_dataset/deu/test.txt"),
-                     'ind': enumerate_lines("/home/data/coco4mt-shared-task/hr_dataset/ind/test.txt"),
-                     'kor': enumerate_lines("/home/data/coco4mt-shared-task/hr_dataset/kor/test.txt"),
-                     'fra': enumerate_lines("/home/data/coco4mt-shared-task/lr_dataset/fra/test.txt"),
-                     'mya': enumerate_lines("/home/data/coco4mt-shared-task/lr_dataset/mya/test.txt"),
-                     'guj': enumerate_lines("/home/data/coco4mt-shared-task/lr_dataset/guj/test.txt")}
+    train_corpora = {'eng': enumerate_lines(f"{DATA_DIR}/coco4mt-shared-task/hr_dataset/eng/train.txt"),
+                     'deu': enumerate_lines(f"{DATA_DIR}/coco4mt-shared-task/hr_dataset/deu/train.txt"),
+                     'ind': enumerate_lines(f"{DATA_DIR}/coco4mt-shared-task/hr_dataset/ind/train.txt"),
+                     'kor': enumerate_lines(f"{DATA_DIR}/coco4mt-shared-task/hr_dataset/kor/train.txt"),
+                     'fra': enumerate_lines(f"{DATA_DIR}/coco4mt-shared-task/lr_dataset/fra/train.txt"),
+                     'mya': enumerate_lines(f"{DATA_DIR}/coco4mt-shared-task/lr_dataset/mya/train.txt"),
+                     'guj': enumerate_lines(f"{DATA_DIR}/coco4mt-shared-task/lr_dataset/guj/train.txt")}
+    valid_corpora = {'eng': enumerate_lines(f"{DATA_DIR}/coco4mt-shared-task/hr_dataset/eng/dev.txt"),
+                     'deu': enumerate_lines(f"{DATA_DIR}/coco4mt-shared-task/hr_dataset/deu/dev.txt"),
+                     'ind': enumerate_lines(f"{DATA_DIR}/coco4mt-shared-task/hr_dataset/ind/dev.txt"),
+                     'kor': enumerate_lines(f"{DATA_DIR}/coco4mt-shared-task/hr_dataset/kor/dev.txt"),
+                     'fra': enumerate_lines(f"{DATA_DIR}/coco4mt-shared-task/lr_dataset/fra/dev.txt"),
+                     'mya': enumerate_lines(f"{DATA_DIR}/coco4mt-shared-task/lr_dataset/mya/dev.txt"),
+                     'guj': enumerate_lines(f"{DATA_DIR}/coco4mt-shared-task/lr_dataset/guj/dev.txt")}
+    test_corpora  = {'eng': enumerate_lines(f"{DATA_DIR}/coco4mt-shared-task/hr_dataset/eng/test.txt"),
+                     'deu': enumerate_lines(f"{DATA_DIR}/coco4mt-shared-task/hr_dataset/deu/test.txt"),
+                     'ind': enumerate_lines(f"{DATA_DIR}/coco4mt-shared-task/hr_dataset/ind/test.txt"),
+                     'kor': enumerate_lines(f"{DATA_DIR}/coco4mt-shared-task/hr_dataset/kor/test.txt"),
+                     'fra': enumerate_lines(f"{DATA_DIR}/coco4mt-shared-task/lr_dataset/fra/test.txt"),
+                     'mya': enumerate_lines(f"{DATA_DIR}/coco4mt-shared-task/lr_dataset/mya/test.txt"),
+                     'guj': enumerate_lines(f"{DATA_DIR}/coco4mt-shared-task/lr_dataset/guj/test.txt")}
     
     data = []
     corpus = train_corpora
@@ -88,6 +90,56 @@ def coco_data(src, tgt, lines=None):
             item = {'id': i, 
                     'translation': {code3[src]: corpus[code3[src]][i],
                                     code3[tgt]: corpus[code3[tgt]][i]}}
+            data.append(item)
+    test_ds = Dataset.from_pandas(DataFrame(data=data))
+    result = DatasetDict()
+    result['train'] = train_ds
+    result['validation'] = valid_ds
+    result['test'] = test_ds
+    return result
+
+
+def nllb_data(src, tgt, lines=None):
+    """Creates a DatasetDict with the NLLB data.
+    
+    If you provide a set of line numbers (as zero-indexed ints) for the
+    lines argument, then this function will subselect only those lines
+    for the training partition (the full validation and test sets are
+    always used).    
+    
+    """
+    
+    data = []
+    subcorpus = enumerate_lines(f'{DATA_DIR}/nllb/parallelized/{src}')
+    target_corpus = enumerate_lines(f'{DATA_DIR}/nllb/parallelized/{tgt}')
+    for i in range(len(subcorpus)):
+        if lines is None or i in lines:
+            if len(subcorpus[i]) > 0 and len(target_corpus[i]) > 0:
+                item = {'id': i, 
+                        'translation': {src: subcorpus[i],
+                                        tgt: target_corpus[i]}}
+                data.append(item)
+    train_ds = Dataset.from_pandas(DataFrame(data=data))
+    
+    data = []
+    subcorpus = enumerate_lines(f'{DATA_DIR}/flores200_dataset/dev/{src}.dev')
+    target_corpus = enumerate_lines(f'{DATA_DIR}/flores200_dataset/dev/{tgt}.dev')
+    for i in range(len(subcorpus)):
+        if len(subcorpus[i]) > 0 and len(target_corpus[i]) > 0:
+            item = {'id': i, 
+                    'translation': {src: subcorpus[i],
+                                    tgt: target_corpus[i]}}
+            data.append(item)
+    valid_ds = Dataset.from_pandas(DataFrame(data=data))
+    
+    data = []
+    subcorpus = enumerate_lines(f'{DATA_DIR}/flores200_dataset/devtest/{src}.devtest')
+    target_corpus = enumerate_lines(f'{DATA_DIR}/flores200_dataset/devtest/{tgt}.devtest')
+    for i in range(len(subcorpus)):
+        if len(subcorpus[i]) > 0 and len(target_corpus[i]) > 0:
+            item = {'id': i, 
+                    'translation': {src: subcorpus[i],
+                                    tgt: target_corpus[i]}}
             data.append(item)
     test_ds = Dataset.from_pandas(DataFrame(data=data))
     result = DatasetDict()
